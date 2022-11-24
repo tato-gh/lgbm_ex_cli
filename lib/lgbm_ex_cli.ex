e72df0df723546089e2f186c0b0175e202798c26
ta.to <61908291+tato-gh@users.noreply.github.com>
2022-11-24 11:44:11 +0900

    Add mix new application

---
+++ lib/lgbm_ex_cli.ex
@@ -0,0 +1,18 @@
+defmodule LGBMExCli do
+  @moduledoc """
+  Documentation for `LGBMExCli`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LGBMExCli.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lgbm_ex_cli_test.exs
@@ -0,0 +1,8 @@
+defmodule LGBMExCliTest do
+  use ExUnit.Case
+  doctest LGBMExCli
+
+  test "greets the world" do
+    assert LGBMExCli.hello() == :world
+  end
+end
